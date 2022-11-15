49270f39e87a2942e1ff2857cd460030ef528f52
Shalil-j <shaliljaiswar462@gmail.com>
2022-11-16 00:48:22 +0530

    login page done

--- js/home.js
+++ js/home.js
@@ -67,21 +67,42 @@ $(document).ready(function() {
 
 // store
 
-	var quantitiy=1;
-	   $('.quantity-plus').click(function(e){
-			e.preventDefault();
-			var quantity = parseInt($('#quantity').val());
-				$('#quantity').val(quantity + 1);
-		});
-	
-		 $('.quantity-minus').click(function(e){
-			e.preventDefault();
-			var quantity = parseInt($('#quantity').val());
-				if(quantity>0){
-				$('#quantity').val(quantity - 1);
-				}
-		});
 
 
 
-  });
+
+
+// login page
+$('#login-slider').owlCarousel({
+	loop: true,
+	autoplay: true,
+	autoplayTimeout: 5000,
+	autoplayHoverPause: true,
+	dots: true,
+	// nav: true,
+	responsiveBaseElement: 'body',
+	responsive: {
+		0: {
+			items: 1,
+		}
+	},
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
